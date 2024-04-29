ff21ee581fd85f3fb91feadb6250f7c897e7aee2
IssaAhmedBCS <issaahmedsuleiman5189@gmail.com>
2024-04-29 10:53:33 +0300

    my-work

---
+++ Question1.java
@@ -0,0 +1,8 @@
+public class Question1 {
+    public static void main(String[] args) {
+        System.out.println("Football: A Beautiful Game");
+        System.out.println("Football, also known as soccer in some countries, is one of the most popular sports globally.");
+        System.out.println("The game is played between two teams, each with eleven players, on a rectangular field called a pitch.");
+        System.out.println("The objective of the game is to score goals by getting the ball into the opposing team's goal.");
+    }
+}
